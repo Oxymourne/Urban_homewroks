37f7864a7aa10b7e98d90f793e7ceac16259883e
Макс <kingmaks90@yandex.ru>
2024-11-12 19:57:32 +0300

    .

---
+++ Urban/module_1_3.py
@@ -0,0 +1,13 @@
+name = 'Максим'
+print(f'Name: {name}')
+
+age = 32
+print(f'Age: {age}')
+
+new_age = age + 10
+print(f'New Age: {new_age}')
+
+is_student = True
+print(f'Is Student: {is_student}')
+
+print('Name:', name)
