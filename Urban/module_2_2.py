09f6146b05d8749371ca3896e0f6a14bdf1b2478
Макс <kingmaks90@yandex.ru>
2024-11-15 00:58:38 +0300

    Склеил 2 elif в 1

--- Urban/module_2_2.py
+++ Urban/module_2_2.py
@@ -4,9 +4,7 @@ third = int(input('Введите третье число: '))
 
 if first == second and first == third:
     print(3)
-elif first == second and first != third or first != second and first == third:
-    print(2)
-elif first != second and second == third:
+elif first == second and first != third or first != second and first == third or first != second and second == third:
     print(2)
 else:
     print(0)
