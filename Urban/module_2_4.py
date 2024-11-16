92f4a8237337fde357acce281dfa6a3dcf71dc93
Макс <kingmaks90@yandex.ru>
2024-11-16 16:01:45 +0300

    .

--- Urban/module_2_4.py
+++ Urban/module_2_4.py
@@ -1,12 +1,23 @@
-my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-index = 0
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
 
-while index <= len(my_list) - 1:    # Вычитаю 1, т.к. индексация с 0, а не с 1
-    if my_list[index] > 0:
-        print(my_list[index])
-        index += 1
-    elif my_list[index] == 0:
-        index += 1
-        continue
+''' Тут внесу немного ясности в циклы. Элементы перебираю в срезе numbers, чтобы исключить 1
+Флаг засунул в первый цикл, чтобы при его изменении он автоматически становился True, когда берем новый элемент
+Также использовал break, как было в не обязательном пункте
+Вывод сделал через f-строки и тройные кавычки, чтобы сохранился формат
+'''
+
+for i in numbers[1::]:
+    is_prime = True
+    for j in range(2, i):
+        if i % j == 0:
+            is_prime = False
+            break
+    if is_prime is True:
+        primes.append(i)
     else:
-        break
+        not_primes.append(i)
+
+print(f'''Primes: {primes}
+Not Primes: {not_primes}''')
