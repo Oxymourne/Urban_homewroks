add040c5c6fdc30f457ae3f02d171491013e3c61
Макс <kingmaks90@yandex.ru>
2024-11-18 15:09:01 +0300

    .

--- Urban/module_2_hard.py
+++ Urban/module_2_hard.py
@@ -2,21 +2,18 @@ n = int(input('Введите число с ворот: '))
 dividers_list = []
 res = []
 
-for i in range(3, n + 1):
+for i in range(3, n + 1):   # Тут создаем список делителей введенного числа
     if n % i == 0:
         dividers_list.append(i)
 
-print(dividers_list)
-
-number = 1
+number = 1  # Первое число пары. Оно всегда меньше n / 2
 while number < n / 2:
-    for j in dividers_list:
-        if number > j // 2:
+    for divider in dividers_list:
+        if number < divider / 2:
+            res.append(str(number))
+            res.append(str(divider - number))
+        else:
             continue
-        res.append(str(number))
-        res.append(str(j - number))
     number += 1
 
-print(''.join(res))
-print('13141911923282183731746416515614713812911')
-print('13141911923282183731746416515614713812911' == ''.join(res))
+print(f'Ваш пароль: {''.join(res)}')
