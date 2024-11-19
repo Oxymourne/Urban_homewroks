bc4331af04c1ddc288940f9ffc8614276e4b416d
Макс <kingmaks90@yandex.ru>
2024-11-19 21:54:06 +0300

    Исправления: 1. Поправил кавычки у ф-строки в принте на двойные 2. Убрал цикл while и перенес вычисление делителя внутрь цикла по поиску первого числа пары

--- Urban/module_2_hard.py
+++ Urban/module_2_hard.py
@@ -1,19 +1,10 @@
 n = int(input('Введите число с ворот: '))
-dividers_list = []
 res = []
 
-for i in range(3, n + 1):   # Тут создаем список делителей введенного числа
-    if n % i == 0:
-        dividers_list.append(i)
-
-number = 1  # Первое число пары. Оно всегда меньше n / 2
-while number < n / 2:
-    for divider in dividers_list:
-        if number < divider / 2:
+for number in range(1, int((n + 1) / 2)):   # Создаем первое числа пары
+    for divider in range(3, n + 1):    # Цикл для делителей числа
+        if n % divider == 0 and number < divider / 2:
             res.append(str(number))
             res.append(str(divider - number))
-        else:
-            continue
-    number += 1
 
-print(f'Ваш пароль: {''.join(res)}')
+print(f"Ваш пароль: {''.join(res)}")
