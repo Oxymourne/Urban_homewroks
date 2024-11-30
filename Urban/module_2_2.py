bfad30fcd9bf79abee254dac8422f2f1810fb978
Макс <kingmaks90@yandex.ru>
2024-11-30 14:10:28 +0300

    .

--- Urban/module_2_2.py
+++ Urban/module_2_2.py
@@ -4,7 +4,7 @@ third = int(input('Введите третье число: '))
 
 if first == second and first == third:
     print(3)
-elif first == second and first != third or first != second and first == third or first != second and second == third:
+elif first == second or first == third or second == third:
     print(2)
 else:
     print(0)
