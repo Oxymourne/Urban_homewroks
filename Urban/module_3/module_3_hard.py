8dd4c700bfc7698f6c4c0887cc6c20f6ca75a43b
Макс <kingmaks90@yandex.ru>
2024-12-01 16:42:46 +0300

    .

---
+++ Urban/module_3/module_3_hard.py
@@ -0,0 +1,35 @@
+data_structure = [
+
+    [1, 2, 3],
+
+    {'a': 4, 'b': 5},
+
+    (6, {'cube': 7, 'drum': 8}),
+
+    "Hello",
+
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
+
+]
+
+
+def calculate_structure_sum(*args):
+    cnt = 0
+    for structure in args:
+        if isinstance(structure, int):
+            cnt += structure
+        elif isinstance(structure, str):
+            cnt += len(structure)
+        elif isinstance(structure, list) or isinstance(structure, tuple) or isinstance(structure, set):
+            for element in structure:
+                cnt += calculate_structure_sum(element)
+        elif isinstance(structure, dict):
+            for key, value in structure.items():
+                cnt += calculate_structure_sum(key, value)
+
+    return cnt
+
+
+result = calculate_structure_sum(data_structure)
+
+print(result)
