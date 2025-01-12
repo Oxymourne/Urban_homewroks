1d829a6c03107b7911b3d2125bc6cf38d5eb87da
Макс <kingmaks90@yandex.ru>
2025-01-12 15:20:12 +0300

    .

--- Urban/Test_git.py
+++ Urban/Test_git.py
@@ -1,6 +1,6 @@
-a = 478480
-coef = 4.4
-res1 = a * coef
-print(res1)
-c = 378480 * 6.6
-print(res1 - c)
+from decimal import Decimal, InvalidOperation
+
+a = 1.2
+b = 123
+
+print(Decimal(a) + Decimal(b))
